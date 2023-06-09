4bae0e5169d12978a16e90d914b51dd2e6a7952a
Callowlock <chris.nevares@yahoo.com>
2023-06-09 16:26:37 -0700

    Added detectCapitalUse method and first test

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -2,22 +2,40 @@ public class CILab implements CILabInterface {
     private String myString;
 
     public void CILab() {
-        myString = null;
+        this.myString = null;
     }
     @Override
     public String getString() {
-        return myString;
+        return this.myString;
     }
 
     @Override
     public void setString(String string) {
-        myString = string;
+        this.myString = string;
     }
 
     @Override
     public boolean detectCapitalUse() {
+        String tempFirstLetter = this.myString.substring(0,1);
+
+        if(this.myString == this.myString.toUpperCase()) {
+            return true;
+        } else if(this.myString == this.myString.toLowerCase()) {
+            return true;
+        } else if(tempFirstLetter == tempFirstLetter.toUpperCase() && containsUpper(myString.substring(1, myString.length()))) {
+            return true;
+        }
         return false;
     }
 
+    private boolean containsUpper(String s) {
+        for(int i = 0; i < s.length(); i++) {
+            String c = s.charAt(i) +"";
+            if(c == c.toUpperCase()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
